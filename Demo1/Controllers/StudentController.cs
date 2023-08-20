edec4471eb1525882aeeb56557f406490a37b767
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-08-20 13:32:44 +0300

    first commit

---
+++ Demo1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Show()
+        {
+            return "Hello MVC";
+        }
+
+        public int Add()
+        {
+            return 20;
+        }
+    }
+}
